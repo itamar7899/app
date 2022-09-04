37377375e9d30998e26d3936cf23570aaa6caf16
itamar7899 <61388486+itamar7899@users.noreply.github.com>
2022-09-04 18:07:18 +0300

    update

--- main.py
+++ main.py
@@ -1,20 +1,2 @@
-from functions import get_ip
-import os
-
-
-list = []
-
-while  :
-    command = input("Insert a Command >>")
-    if command == "exit":
-        break
-    list.append(command)
-print(list)
-
-for n in list:
-    output = os.popen(n).read()
-    print(output)
-
-
-print(get_ip())
+print("hello world")
 
